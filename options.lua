acad3bce10ad7dec933f2d5479d08276970d9198
Riley Castelli <castelliriley@gmail.com>
2023-05-24 19:49:36 -0400

    fix clipboard

--- options.lua
+++ options.lua
@@ -7,7 +7,7 @@ return {
     spell = false,         -- sets vim.opt.spell
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = false,          -- sets vim.opt.wrap
-    clipboard = 'unnamed'
+    clipboard = 'unnamedplus'
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
